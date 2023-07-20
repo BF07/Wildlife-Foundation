ce5bb8e958e127c350902bb5b2cf286ffb3a2aad
Bruno Fina <bruno-fina@hotmail.fr>
2023-07-20 15:12:28 +0400

    Create JS scrolling animation

--- JS/script.js
+++ JS/script.js
@@ -13,4 +13,33 @@ btnToggler.addEventListener('click', function() {
     footer.style.opacity = '1';
     nombreDeClics = 0;
    }
+});
+
+/* Apparition au défilement */
+
+const ratio = .3;
+
+const options = {
+   root: null,
+   rootMargin: '0px',
+   threshold: ratio
+};
+
+const handleIntersect = function (entries, observer) {
+   entries.forEach(function (entry) {
+      if (entry.intersectionRatio > ratio) {
+         entry.target.classList.add('reveal-visible');
+         observer.unobserve(entry.target);
+      }
+   })
+};
+
+document.documentElement.classList.add('reveal-loaded');
+window.addEventListener('DOMContentLoaded', function () {
+   const observer = new IntersectionObserver(handleIntersect, options);
+   document.querySelectorAll('[class*= "reveal-"]').forEach(function (r) {
+      observer.observe(r);
+   });
 })
+
+/* Fin apparition au défilement */
